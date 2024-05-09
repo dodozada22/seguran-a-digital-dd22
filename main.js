f5faaae1199031ce2d21d59af7f1407f45469298
dodozada22 <161779505+dodozada22@users.noreply.github.com>
2024-05-09 14:23:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,13 +18,18 @@ function diminuiTamanho() {
         // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
+    
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
+
+    
 }
 function aumentaTamanho() {
+    
     if (tamanhoSenha < 20) {
         // tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
+        
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
